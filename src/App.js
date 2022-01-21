95ad5b4ca1e23666bfe721a697a4904f49124a0b
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-20 21:59:57 -0500

    built out reducer, actions, and connected fetching action to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
+import { fetchSmurfs } from './actions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
@@ -20,7 +22,8 @@ const App = ()=> {
   );
 }
 
-export default App;
+
+export default connect(null, { fetchSmurfs } )(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
